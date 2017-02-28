e340eef5bd8547322298363d353a7291351fbfa0
Yorso <jorgeguzon@gmail.com>
2017-02-28 12:18:32 +0100

    comments added

--- microspring7-word-server-feign-lab6/src/main/java/com/jorge/WordController.java
+++ microspring7-word-server-feign-lab6/src/main/java/com/jorge/WordController.java
@@ -13,10 +13,11 @@ public class WordController {
 	//String words = "icicle,refrigerator,blizzard,snowball"; //Uncomment this line to test "BONUS - Multiple Clients"
 	
 	@RequestMapping("/")
-	public @ResponseBody Word getWord() {
+	//public @ResponseBody String getWord() { //Old way until now: returning a string, not a Word => "use the word server from lab 6. It has been refactored to return a Word, not a String"
+	public @ResponseBody Word getWord() { //Now, for this example with Feign: returning a Word, not a string
 		String[] wordArray = words.split(",");
 		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
-		//return wordArray[i]; Old way until now: returning a string, not a Word => "use the word server from lab 6. It has been refactored to return a Word, not a String"
+		//return wordArray[i]; //Old way until now: returning a string, not a Word => "use the word server from lab 6. It has been refactored to return a Word, not a String"
 		return new Word(wordArray[i]); //Now, for this example with Feign: returning a Word, not a string
 	}
 }
